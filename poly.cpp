42eb7d566408e4c9353930532d8f0a07e875a942
arsul jawed <128362584+arsuljawed12@users.noreply.github.com>
2024-03-18 12:47:16 +0530

    Add files via upload

---
+++ poly.cpp
@@ -0,0 +1,21 @@
+#include<graphics.h>
+//#define HEIGHT 800
+//#define WIDTH 800
+
+int main()
+{
+    //initwindow(WIDTH,HEIGHT,"shape demo");
+
+    int gd= DETECT,gm;
+    int arr[] = {320,150,
+                 400,250,
+                 250,350,
+                 320,150};
+    initgraph(&gd,&gm,"");
+    drawpoly(4,arr);
+    getch();
+    closegraph();
+
+    return 0;
+
+}
